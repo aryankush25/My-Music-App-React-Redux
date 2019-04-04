a87621b937850df77f71235637f34ee65c3b37a7
Aryan Agarwal <aryan@geekyants.com>
2019-04-04 14:41:53 +0530

    Project Setup - Folder Setup

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,34 @@
 import React, { Component } from 'react';
 import 'bootstrap/dist/css/bootstrap.css';
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import Index from "../routes/Index";
+import About from "../routes/About";
+import Users from "../routes/Users";
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <h1>Ready and connected</h1>
-      </div>
+      <Router>
+        <div>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/about/">About</Link>
+              </li>
+              <li>
+                <Link to="/users/">Users</Link>
+              </li>
+            </ul>
+          </nav>
+  
+          <Route path="/" exact component={Index} />
+          <Route path="/about/" component={About} />
+          <Route path="/users/" component={Users} />
+        </div>
+      </Router>
     );
   }
 }
--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './components/index.css';
 import App from './components/App';
 import 'bootstrap/dist/css/bootstrap.css';
-import './custom.scss';
+import './assets/styles/custom.scss';
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 ReactDOM.render(<App />, document.getElementById('root'));
