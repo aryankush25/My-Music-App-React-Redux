0bd703666d68c1999e07cda125cd9c8873a266db
Aryan Agarwal <aryan@geekyants.com>
2019-04-04 12:56:23 +0530

    Project Setup - React, React Router & Bootstrap with sass

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import 'bootstrap/dist/css/bootstrap.css';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Ready and connected</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
