f057418f944f58ff8d77a668a744a9052dcb697d
Aryan Agarwal <aryan@geekyants.com>
2019-04-06 17:36:42 +0530

    Firebase Conffig added

--- src/index.js
+++ src/index.js
@@ -1,10 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import firebase from "firebase";
 import "./index.scss";
 import App from "./components/App";
 import { BrowserRouter as Router } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.css";
 import "./assets/styles/custom.scss";
+
+var config = {
+  apiKey: "AIzaSyCt9gbRk3r0d7fLBFAcgOVNgGFwayc2wug",
+  authDomain: "react-mini-project-music-app.firebaseapp.com",
+  databaseURL: "https://react-mini-project-music-app.firebaseio.com",
+  projectId: "react-mini-project-music-app",
+  storageBucket: "react-mini-project-music-app.appspot.com",
+  messagingSenderId: "217762414151"
+};
+firebase.initializeApp(config);
 
 ReactDOM.render(
   <Router>
