ad08f0c7a736c40ee1b5e4d44519a902055feadf
Aryan Agarwal <aryan@geekyants.com>
2019-04-25 20:21:23 +0530

    Reactstrap Added

--- src/components/Playlists/AddPlaylist.js
+++ src/components/Playlists/AddPlaylist.js
@@ -37,7 +37,37 @@ class AddPlaylist extends React.Component {
     }
     return (
       <div className="button-class">
-        <button onClick={() => this.handleAddPlaylist()}>Add Playlist</button>
+        {/* <button onClick={() => this.handleAddPlaylist()}>Add Playlist</button> */}
+        <button
+          type="button"
+          className="btn btn-info btn-lg"
+          data-toggle="modal"
+          data-target="#myModal"
+        >
+          Open Modal
+        </button>
+        <div className="modal fade" id="myModal" role="dialog">
+          <div className="modal-dialog">
+            <div className="modal-content">
+              <div className="modal-header">
+                <button type="button" className="close" data-dismiss="modal" />
+                <h4 className="modal-title">Modal Header</h4>
+              </div>
+              <div className="modal-body">
+                <p>Some text in the modal.</p>
+              </div>
+              <div className="modal-footer">
+                <button
+                  type="button"
+                  className="btn btn-default"
+                  data-dismiss="modal"
+                >
+                  Close
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
