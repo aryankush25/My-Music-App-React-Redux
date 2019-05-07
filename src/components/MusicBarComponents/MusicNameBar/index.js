54fd4455aedad0788864c1bf53219ae391eab7c8
Aryan Agarwal <aryan@geekyants.com>
2019-05-07 13:04:07 +0530

    Song Name And Image Shown On Music Bar

--- src/components/MusicBarComponents/MusicNameBar/index.js
+++ src/components/MusicBarComponents/MusicNameBar/index.js
@@ -1,18 +1,61 @@
 import React from "react";
 import { connect } from "react-redux";
+import "./style.scss";
+
+class MusicImage extends React.Component {
+  render() {
+    if (this.props.currentSongImage !== "") {
+      return <img src={this.props.currentSongImage} alt="User-Img" />;
+    } else {
+      return (
+        <div className="song-image-bar">
+          <p>
+            {this.props.currentSongName
+              ? this.props.currentSongName.trim(0).charAt(0)
+              : "?"}
+          </p>
+        </div>
+      );
+    }
+  }
+}
 
 class MusicNameBar extends React.Component {
   render() {
-    return <p>{this.props.currentSongName}</p>;
+    if (this.props.songsArrayLength === 0) {
+      return (
+        <div className="song-name-music-bar">
+          <p>Please Upload a Song</p>
+        </div>
+      );
+    }
+
+    return (
+      <div className="song-name-music-bar">
+        <MusicImage
+          currentSongImage={this.props.currentSongImage}
+          currentSongName={this.props.currentSongName}
+        />
+        <p>{this.props.currentSongName}</p>
+      </div>
+    );
   }
 }
 
 const mapStateToProps = state => {
   const { songArray, songNumber } = state.song;
 
+  var songsArrayLength = songArray.length;
+
+  if (songsArrayLength === 0) {
+    return { songsArrayLength };
+  }
+
   var currentSongName = songArray[songNumber].name;
 
-  return { currentSongName };
+  var currentSongImage = songArray[songNumber].imageUrl;
+
+  return { currentSongName, currentSongImage };
 };
 
 export default connect(mapStateToProps)(MusicNameBar);
