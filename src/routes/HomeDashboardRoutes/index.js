bd6bc55e8d42be33b3b598d1b7ec86f4837da4e0
Aryan Agarwal <aryan@geekyants.com>
2019-05-07 14:40:40 +0530

    MusicBarComponent Maded

--- src/routes/HomeDashboardRoutes/index.js
+++ src/routes/HomeDashboardRoutes/index.js
@@ -1,184 +1,22 @@
 import React from "react";
-import { Howl, Howler } from "howler";
 import HomePageDashboard from "../../components/HomePageDashboard";
-import MusicSeekBar from "../../components/MusicBarComponents/MusicSeekBar";
-import MusicVolumeBar from "../../components/MusicBarComponents/MusicVolumeBar";
-import MusicBarButtons from "../../components/MusicBarComponents/MusicBarButtons";
-import MusicNameBar from "../../components/MusicBarComponents/MusicNameBar/";
-import ShowLoadingComponent from "../../components/ShowLoadingComponent";
+import MusicBar from "../../components/MusicBarComponents/";
 import "./style.scss";
 import { connect } from "react-redux";
-import {
-  setSongAction,
-  setCurrentSongNumberAction,
-  setSongIsLoadingAction,
-  setSongIsPlayingAction,
-  setSongCurrentDurationAction,
-  setIsNewSongAction,
-  setSongAndPlayAction,
-  setSongStopAction
-} from "../../redux/actions/actionSongs";
+
+const MusicBarComponent = props => {
+  if (props.arrayIsEmpty) {
+    return (
+      <div className="music-bar">
+        <p>Please Upload The Song</p>
+      </div>
+    );
+  }
+
+  return <MusicBar />;
+};
 
 class Home extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      currentSongDuration: 0
-    };
-  }
-
-  componentDidUpdate(prevProps) {
-    function isEmpty(obj) {
-      for (var prop in obj) {
-        if (obj.hasOwnProperty(prop)) return false;
-      }
-      return true;
-    }
-
-    if (this.props.stopSong === true) {
-      if (!isEmpty(this.sound)) {
-        this.sound.stop();
-        clearInterval(this.intervalID);
-        this.props.setSongIsPlayingAction(false);
-        this.props.setIsNewSongAction(true);
-        this.props.setSongCurrentDurationAction(0);
-        this.props.setSongStopAction(false);
-      }
-    }
-
-    if (prevProps.isNewSong && this.props.isNewSong === true) {
-      if (!isEmpty(this.sound)) {
-        this.sound.stop();
-        clearInterval(this.intervalID);
-      }
-
-      this.sound = new Howl({
-        src: [this.props.songArray[this.props.songNumber].url],
-        html5: true
-      });
-    }
-
-    if (prevProps.isNewSong === false && this.props.isNewSong === true) {
-      if (!isEmpty(this.sound)) {
-        this.sound.stop();
-        clearInterval(this.intervalID);
-      }
-
-      this.sound = new Howl({
-        src: [this.props.songArray[this.props.songNumber].url],
-        html5: true
-      });
-
-      if (this.props.isPlaying === true) {
-        this.intervalID = setInterval(this.handleSongTimer, 1000);
-        this.sound.play();
-      }
-
-      this.props.setIsNewSongAction(false);
-    }
-
-    if (prevProps.isPlaying === false && this.props.isPlaying === true) {
-      if (this.props.isNewSong === true) {
-        this.sound = new Howl({
-          src: [this.props.songArray[this.props.songNumber].url],
-          html5: true
-        });
-        this.props.setIsNewSongAction(false);
-      }
-      this.intervalID = setInterval(this.handleSongTimer, 1000);
-      this.sound.play();
-    }
-  }
-
-  componentWillUnmount = () => {
-    console.log("Component Unmounted");
-  };
-
-  sound = {};
-  intervalID = 0;
-
-  handleSongTimer = () => {
-    if (Math.round(this.sound._duration) === this.props.currentSongDuration) {
-      this.handlePlayNext();
-    } else {
-      this.props.setSongCurrentDurationAction(
-        this.props.currentSongDuration + 1
-      );
-    }
-  };
-
-  handlePlayAudio = () => {
-    this.props.setSongIsPlayingAction(true);
-  };
-
-  handlePauseAudio = () => {
-    this.sound.pause();
-    clearInterval(this.intervalID);
-    this.props.setSongIsPlayingAction(false);
-  };
-
-  handlePlayPauseAudio = () => {
-    if (this.props.isPlaying === true) {
-      this.handlePauseAudio();
-    } else {
-      this.handlePlayAudio();
-    }
-  };
-
-  handlePlayNext = async () => {
-    var songNumber = 0;
-    if (this.props.songArray.length - 1 > this.props.songNumber) {
-      songNumber = this.props.songNumber + 1;
-    } else {
-      songNumber = 0;
-    }
-
-    this.sound.stop();
-    clearInterval(this.intervalID);
-
-    this.props.setSongAndPlayAction(songNumber);
-  };
-
-  handlePlayPrevious = () => {
-    var songNumber = 0;
-    if (this.props.songNumber > 0) {
-      songNumber = this.props.songNumber - 1;
-    } else {
-      songNumber = this.props.songArray.length - 1;
-    }
-
-    this.sound.stop();
-    clearInterval(this.intervalID);
-
-    this.props.setSongAndPlayAction(songNumber);
-  };
-
-  handleAdjustSeek = value => {
-    this.sound.seek(value);
-    this.props.setSongCurrentDurationAction(value);
-  };
-
-  handleAdjustAudio = value => {
-    Howler.volume(value / 10);
-  };
-
-  printProps = () => {
-    const {
-      songArray,
-      songNumber,
-      isPlaying,
-      currentSongDuration,
-      isNewSong
-    } = this.props;
-    console.log({
-      songArray,
-      songNumber,
-      isPlaying,
-      currentSongDuration,
-      isNewSong
-    });
-  };
-
   render() {
     return (
       <div className="home-container">
@@ -187,26 +25,7 @@ class Home extends React.Component {
 
           {/* Music Bar Div That Contains the music bar elements */}
 
-          <ShowLoadingComponent isLoading={this.props.isLoadingSong}>
-            <div className="music-bar">
-              <MusicNameBar />
-              <MusicBarButtons
-                isPlaying={this.props.isPlaying}
-                playPrevious={this.handlePlayPrevious}
-                playPauseAudio={this.handlePlayPauseAudio}
-                playNext={this.handlePlayNext}
-              />
-              <MusicSeekBar
-                duration={this.sound._duration}
-                currentDuration={this.props.currentSongDuration}
-                adjustSeek={this.handleAdjustSeek}
-              />
-              <MusicVolumeBar
-                volume={Howler._volume}
-                adjustAudio={this.handleAdjustAudio}
-              />
-            </div>
-          </ShowLoadingComponent>
+          <MusicBarComponent arrayIsEmpty={this.props.arrayIsEmpty} />
         </div>
       </div>
     );
@@ -214,51 +33,16 @@ class Home extends React.Component {
 }
 
 const mapStateToProps = state => {
-  const {
-    songArray,
-    songNumber,
-    isPlaying,
-    currentSongDuration,
-    isNewSong,
-    isLoading: isLoadingSong
-  } = state.song;
+  const { songArray } = state.song;
+
+  var arrayIsEmpty = false;
+  if (songArray.length === 0) {
+    arrayIsEmpty = true;
+  }
 
   return {
-    songArray,
-    songNumber,
-    isPlaying,
-    currentSongDuration,
-    isNewSong,
-    isLoadingSong
+    arrayIsEmpty
   };
 };
 
-const mapDispatchToProps = dispatch => {
-  return {
-    setSongAction: songsArray => dispatch(setSongAction(songsArray)),
-
-    setCurrentSongNumberAction: songNumber =>
-      dispatch(setCurrentSongNumberAction(songNumber)),
-
-    setSongIsLoadingAction: isLoading =>
-      dispatch(setSongIsLoadingAction(isLoading)),
-
-    setSongIsPlayingAction: isPlaying =>
-      dispatch(setSongIsPlayingAction(isPlaying)),
-
-    setSongCurrentDurationAction: currentSongDuration =>
-      dispatch(setSongCurrentDurationAction(currentSongDuration)),
-
-    setIsNewSongAction: isNewSong => dispatch(setIsNewSongAction(isNewSong)),
-
-    setSongAndPlayAction: songNumber =>
-      dispatch(setSongAndPlayAction(songNumber)),
-
-    setSongStopAction: stopSong => dispatch(setSongStopAction(stopSong))
-  };
-};
-
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(Home);
+export default connect(mapStateToProps)(Home);
